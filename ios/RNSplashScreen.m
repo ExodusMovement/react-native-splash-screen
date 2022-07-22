011b5d43fc4eb987b2df8221fd1e8062411eec27
Ariene Maiara Ribeiro <ariene.maiara@hotmail.com>
2022-07-22 09:21:17 -0300

    refactor: use player variable
    Co-authored-by: Andrej <andrej-borstnik@hotmail.com>

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -57,7 +57,7 @@ NSString* RNSplashScreenOverlayName = @"splashscreenVideo";
   NSNumber *startSecond = config[@"startSecond"];
   if (startSecond != nil) {
     CMTime newTime = CMTimeMakeWithSeconds([startSecond floatValue], 1);
-    [lastPlayer seekToTime:newTime];
+    [player seekToTime:newTime];
   }
 
   loop = config[@"loopVideo"];
