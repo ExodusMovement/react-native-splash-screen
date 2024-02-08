9a784151a6b1068a651e051a76dfe468b4bf4859
Alexander Pataridze <s.pataridze@gmail.com>
2024-02-08 18:42:47 +0400

    refactor: remove unused changes

--- ios/RNSplashScreen.m
+++ ios/RNSplashScreen.m
@@ -19,8 +19,6 @@ static UIView* splash = nil;
 
 RCT_EXPORT_MODULE(SplashScreen)
 
-NSInteger const RNSplashScreenOverlayTag = 39293;
-
 + (void)show {
   if (splash != nil) return;
   UIViewController *sb = [[UIStoryboard storyboardWithName:@"Launch Screen" bundle:nil] instantiateInitialViewController];
@@ -33,29 +31,6 @@ NSInteger const RNSplashScreenOverlayTag = 39293;
     [splash removeFromSuperview];
     splash = nil;
   }
-}
-
-+ (NSString *)launchImageNameForOrientation:(UIDeviceOrientation)orientation {
-  CGSize viewSize = [[UIScreen mainScreen] bounds].size;
-  NSString* viewOrientation = @"Portrait";
-  if (UIDeviceOrientationIsLandscape(orientation)) {
-    viewSize = CGSizeMake(viewSize.height, viewSize.width);
-    viewOrientation = @"Landscape";
-  }
-
-  NSArray* imagesDict = [[[NSBundle mainBundle] infoDictionary] valueForKey:@"UILaunchImages"];
-  NSString* fallback = nil;
-  for (NSDictionary* dict in imagesDict) {
-    CGSize imageSize = CGSizeFromString(dict[@"UILaunchImageSize"]);
-    NSString *imageName = dict[@"UILaunchImageName"];
-    if (CGSizeEqualToSize(imageSize, viewSize) && [viewOrientation isEqualToString:dict[@"UILaunchImageOrientation"]])
-      return imageName;
-
-    // TODO: find best matching fallback image
-    if (fallback == nil) fallback = imageName;
-  }
-
-  return fallback;
 }
 
 RCT_EXPORT_METHOD(hide) {
